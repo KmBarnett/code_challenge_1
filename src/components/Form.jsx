c521abc8a5eec5209427bd353118ff232801d768
Kyle Barnett <KmBarnett94@gmail.com>
2021-03-23 10:46:45 -0600

    add validation to from as well as some state control

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,48 +1,120 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEnabled: false,
+      name: '',
+      lat: '',
+      lng: '',
+      nameErr: '',
+      latErr: '',
+      lngErr: '',
+    };
+  }
+
   submitForm(e, data) {
     e.preventDefault();
-    this.props.saveLocation(data);
+
+    const { name, lat, lng } = this.state;
+
+    this.props.saveLocation({ name, lat, lng });
+  }
+
+  isInvalidLonLat(value) {
+    return !/^(-?|\+?)?\d+(\.\d+)?$/.test(value);
+  }
+
+  validate() {
+    const { name, lat, lng } = this.state;
+    this.setState({
+      isEnabled:
+        !!name && !this.isInvalidLonLat(lng) && !this.isInvalidLonLat(lat),
+    });
+  }
+
+  handleInputBlur(e, state, msg, condition) {
+    if (!e.target.value) {
+      this.setState({
+        [state]: 'This field is required it cannot be left blank',
+      });
+    } else if  (condition) {
+      this.setState({
+        [state]: msg,
+      });
+    } else {
+      this.setState({ [state]: '' });
+    }
+    this.validate();
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    this.validate();
+  }
+
+  handleLatLangBlur(e, state, field) {
+    const msg = `This not a valid ${field}`;
+    this.handleInputBlur(e, state, msg, this.isInvalidLonLat(e.target.value));
   }
 
   render() {
     return (
       <form className="form">
-        <label>
-          Name
-          <input
-            ref={(input) => { this.name = input }}
-            type="text"
-          />
-        </label>
-        <label>
-          Lat
-          <input
-            ref={(input) => { this.lat = input }}
-            type="text"
-          />
-        </label>
-        <label>
-          Lon
-          <input
-            ref={(input) => { this.lng = input }}
-            type="text"/>
-        </label>
+        <section>
+          <p>{this.state.nameErr}</p>
+          <label>
+            Name*
+            <input
+              onChange={(e) => this.handleInputChange(e)}
+              value={this.state.name}
+              type="text"
+              name="name"
+              onBlur={(e) => {
+                this.handleInputBlur(e, 'nameErr');
+              }}
+            />
+          </label>
+        </section>
+        <section>
+          <p>{this.state.latErr}</p>
+          <label>
+            Lat*
+            <input
+              onChange={(e) => this.handleInputChange(e)}
+              value={this.state.lat}
+              type="text"
+              name="lat"
+              onBlur={(e) => this.handleLatLangBlur(e, 'latErr', 'Latitude')}
+            />
+          </label>
+        </section>
+        <section>
+          <p>{this.state.lngErr}</p>
+          <label>
+            Lon*
+            <input
+              onChange={(e) => this.handleInputChange(e)}
+              value={this.state.lng}
+              type="text"
+              name="lng"
+              onBlur={(e) => this.handleLatLangBlur(e, 'lngErr', 'Longitude')}
+            />
+          </label>
+        </section>
         <button
+          disabled={!this.state.isEnabled}
           type="submit"
-          onClick={(e) => this.submitForm(e, {
-            name: this.name.value,
-            lat: this.lat.value,
-            lng: this.lng.value
-          })}
+          onClick={(e) => this.submitForm(e)}
         >
-            Save
+          Save
         </button>
       </form>
     );
   }
 }
-
 
 export default Form;
