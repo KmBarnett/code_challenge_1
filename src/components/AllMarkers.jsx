629a12d654ae7059f082415709763430efaf2c9f
Kyle Barnett <KmBarnett94@gmail.com>
2021-03-24 08:58:40 -0600

    add set center to props to make map pan to newest entry

--- src/components/AllMarkers.jsx
+++ src/components/AllMarkers.jsx
@@ -3,16 +3,18 @@ import MapMarker from './Marker';
 
 class AllMarkers extends PureComponent {
   render() {
-    const { locations } = this.props;
+    const { locations, setCenter } = this.props;
     const markerArray = locations.map((marker, i) => {
       return (
         <MapMarker
           key={i}
           location={[+marker.lat, +marker.lng]}
-          name={marker.name}
+          name={marker.name}  
         />
       );
     });
+
+    setCenter(locations[locations.length -1])
 
     return <div className="paths-container">{markerArray}</div>;
   }
