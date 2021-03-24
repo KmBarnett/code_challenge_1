21a1cb68b886e514c3a3f345f761bf14cade7bcb
Kyle Barnett <KmBarnett94@gmail.com>
2021-03-24 09:00:50 -0600

    Add auto pan to last point entered

--- src/containers/LeafletMap.js
+++ src/containers/LeafletMap.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, ZoomControl } from 'react-leaflet';
-import AllMarkers from './AllMarkers'
+import AllMarkers from './AllMarkers';
 
 class LeafletMap extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
 
     };
   }
+
+  setCenter = (newCenter) => {
+    if (newCenter) {
+      this.setState({ center: [newCenter.lat, newCenter.lng] });
+    }
+  };
+
   render() {
     return (
       <div className="map-container">
         <Map
           className="map"
           zoomControl={false}
-          center={[39.750809, -104.996810]}
+          center={this.state.center}
           zoom={4}
-          maxBounds={[[85, 100], [-85, -280]]}
+          maxBounds={[
+            [85, 100],
+            [-85, -280],
+          ]}
         >
           <TileLayer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
@@ -25,10 +35,8 @@ class LeafletMap extends Component {
             maxZoom={10}
             minZoom={2}
           />
-          <ZoomControl
-            position="bottomright"
-          />
-          <AllMarkers />
+          <ZoomControl position="bottomright" />
+          <AllMarkers setCenter={this.setCenter} />
         </Map>
       </div>
     );
